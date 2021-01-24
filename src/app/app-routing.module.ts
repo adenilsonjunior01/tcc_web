ae93218d69680fb9ba475c00a6a9ad7eca80b0ee
Adenilson Junior <adenilsonn.souza@gmail.com>
2021-01-24 09:58:38 -0300

    chore(initial): configurando o Projeto

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { Shell } from '@app/shell/shell.service';
+
+//  router-outlet principal,
+const routes: Routes = [
+  Shell.childRoutes([
+    {
+      path: 'about', loadChildren: () => import('./about/about.module').then((m) => m.AboutModule)
+    }
+  ]),
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  exports: [RouterModule],
+  providers: [],
+})
+export class AppRoutingModule {}
