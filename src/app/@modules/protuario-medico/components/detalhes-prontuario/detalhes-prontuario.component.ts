6e0e7f838ea5125fa2e8b77a05f7ceacc88adbf9
Adenilson Junior <adenilsonn.souza@gmail.com>
2021-06-09 23:39:43 -0300

    chore: ajustes no mapeamento no prontuário do Paciente

--- src/app/@modules/protuario-medico/components/detalhes-prontuario/detalhes-prontuario.component.ts
+++ src/app/@modules/protuario-medico/components/detalhes-prontuario/detalhes-prontuario.component.ts
@@ -1,4 +1,14 @@
-import { Component, Input, OnInit, OnDestroy, ViewChild, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  OnDestroy,
+  ViewChild,
+  EventEmitter,
+  Output,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 import { MatAccordion } from '@angular/material/expansion';
 import { UtilitariosService } from '@app/services/utilitarios/utilitarios.service';
 
@@ -7,18 +17,27 @@ import { UtilitariosService } from '@app/services/utilitarios/utilitarios.servic
   templateUrl: './detalhes-prontuario.component.html',
   styleUrls: ['./detalhes-prontuario.component.scss'],
 })
-export class DetalhesProntuarioComponent implements OnInit, OnDestroy {
+export class DetalhesProntuarioComponent implements OnInit, OnDestroy, OnChanges {
   @ViewChild(MatAccordion) accordion: MatAccordion;
   @Input() prontuario: any;
   @Output() closeModal = new EventEmitter();
   public step: number;
   public loadingDownload = false;
+  public prontuarioSelecionado: any;
 
   constructor(private readonly _utilitariosService: UtilitariosService) {}
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.prontuario) {
+      this.prontuarioSelecionado = this.prontuario;
+    }
+  }
+
   ngOnDestroy(): void {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.prontuario);
+  }
 
   public setStep(index: number) {
     this.step = index;
